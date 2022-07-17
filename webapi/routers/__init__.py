7de0269f4479ea7cc780661ec3e4ad1db6a0fe54
luck_baby <67427627+ranyong1997@users.noreply.github.com>
2022-07-17 11:09:09 +0800

    feat(Intervention fastapi):介入hr接口并跑成功
    {
      "project_meta": {
        "debugtalk_py": "",
        "debugtalk_path": "",
        "dot_env_path": "",
        "env": {},
        "RootDir": "/Users/ranyong/Desktop/gitpush/Sakura_Admin/webapi"
      },
      "testcase": {
        "config": {
          "name": "string",
          "verify": false,
          "base_url": "http://127.0.0.1:8001",
          "variables": {},
          "parameters": {},
          "export": [],
          "path": "string",
          "weight": 1
        },
        "teststeps": [
          {
            "name": "string",
            "request": {
              "method": "POST",
              "url": "/api/admin/login/access_token/",
              "params": {},
              "headers": {"content-type":"application/x-www-rorm-urlencoded"},
              "json": {},
              "data": {"username":"admin","password":"123456"},
              "cookies": {},
              "timeout": 120,
              "allow_redirects": true,
              "verify": false,
              "upload": {}
            },
            "variables": {},
            "setup_hooks": [],
            "teardown_hooks": [],
            "extract": {},
            "export": [],
            "validate": [],
            "validate_script": []
          }
        ]
      }
    }

--- webapi/routers/__init__.py
+++ webapi/routers/__init__.py
@@ -12,9 +12,11 @@ from . import category_router  # 导入分类路由
 from . import post_router  # 导入post路由
 from . import user_router  # 导入用户路由
 from . import comment_router  # 导入评论路由
+from . import hr_router  # 导入httprunner路由
 
 api_router = APIRouter()
 api_router.include_router(user_router.router, tags=['用户'], prefix='/admin')  # 挂载用户路由
 api_router.include_router(post_router.router, tags=['发布'], prefix='/posts')  # 挂载post路由
 api_router.include_router(category_router.router, tags=['类别'], prefix='/categories')  # 挂载分类路由
 api_router.include_router(comment_router.router, tags=['评论'], prefix='/comments')  # 挂载评论路由
+api_router.include_router(hr_router.router, tags=['debug'], prefix='/hr')  # 挂载httprunner路由
