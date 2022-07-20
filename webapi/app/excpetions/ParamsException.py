3ad5b94435c5a60e234a2be2976970fe70824941
luck_baby <67427627+ranyong1997@users.noreply.github.com>
2022-07-20 17:21:19 +0800

    🎈feat(Added Redis client management):新增redis客户端管理

---
+++ webapi/app/excpetions/ParamsException.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/7/20 11:36
+# @Author  : 冉勇
+# @Site    : 
+# @File    : ParamsException.py
+# @Software: PyCharm
+# @desc    : 传参错误
+class ParamsError(ValueError):
+    pass
