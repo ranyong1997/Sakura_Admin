5ac59a87789086f70ed52d4147a6270fd75c1329
luck_baby <67427627+ranyong1997@users.noreply.github.com>
2022-07-22 18:01:38 +0800

    🎈feat(Initialize the log system and response status code):初始化日志系统、响应状态码

--- webapi/app/__init__.py
+++ webapi/app/__init__.py
@@ -6,3 +6,94 @@
 # @File    : __init__.py.py
 # @Software: PyCharm
 # @desc    :
+import logging
+import os
+import sys
+import traceback
+from pprint import pformat
+from fastapi import FastAPI, Request, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from loguru import logger
+from loguru._defaults import LOGURU_FORMAT
+from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware.errors import ServerErrorMiddleware
+from starlette.types import Message
+from starlette_context import middleware, plugins
+from webapi.app.excpetions.RequestException import AuthException
+from webapi.app.excpetions.RequestException import PermissionException
+from webapi.config import Config
+
+sakura = FastAPI()
+
+sakura.add_middleware(
+    middleware.ContextMiddleware,
+    plugins={
+        plugins.ForwardedForPlugin()
+    }
+)
+
+# 配置日志格式
+INFO_FORMAT = "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> " \
+              "| <level>{level: <8}</level> | <cyan>文件: {extra[filename]}</cyan> " \
+              "| 模块: <cyan>{extra[business]}</cyan> | 方法: <cyan>{extra[func]}</cyan> " \
+              "| <cyan>行数: {extra[line]}</cyan> | - <level>{message}</level>"
+
+ERROR_FORMAT = "<red>{time:YYYY-MM-DD HH:mm:ss.SSS}</red> " \
+               "| <level>{level: <8}</level> | <cyan>文件: {extra[filename]}</cyan> " \
+               "| 模块: <cyan>{extra[business]}</cyan> | 方法: <cyan>{extra[func]}</cyan> " \
+               "| <cyan>行数: {extra[line]}</cyan> | - <level>{message}</level>"
+
+
+async def set_body(request: Request, body: bytes):
+    async def receive() -> Message:
+        return {"type": "http.request", "body": body}
+
+    request._receive = receive
+
+
+async def get_body(request: Request) -> bytes:
+    body = await request.body()
+    await set_body(request, body)
+    return body
+
+
+@sakura.middleware("http")
+async def errors_handler(request: Request, call_next):
+    try:
+        return await call_next(request)
+    except Exception as exc:
+        traceback.print_exc()
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=jsonable_encoder({
+                "code": 110,
+                "msg": str(exc)
+            })
+        )
+
+
+def error_map(error_type: str, field: str, msg: str = None):
+    if "missing" in error_type:
+        return f"缺少参数:{field}"
+    elif "params" in error_type:
+        return f"参数{field}{'不规范' if msg is None else msg}"
+    elif "not_allowed" in error_type:
+        return f"参数:{field}类型不正确"
+    elif "type_error" in error_type:
+        return f"参数:{field}类型不合法"
+
+
+@sakura.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content=jsonable_encoder({
+            "code": 101,
+            "msg": error_map(exc.errors()[0]["type"], exc.errors()[0].get("loc", ['unknown'])[-1],
+                             exc.errors()[0].get("msg")) if len(exc.errors()) > 0 else "参数解析失败"
+        })
+    )
+
+
