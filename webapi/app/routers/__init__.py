54dc0ace8310420de8dd407e62a94378a3da5e6f
luck_baby <67427627+ranyong1997@users.noreply.github.com>
2022-07-22 16:46:21 +0800

    🎈feat(Added Route initialization):新增路由初始化->鉴权身份

---
+++ webapi/app/routers/__init__.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/7/20 11:40
+# @Author  : 冉勇
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: PyCharm
+# @desc    : 路由初始化[身份鉴权]
+from fastapi import Header
+from starlette import status
+from webapi.app.crud.auth.UserDao import UserDao
+from webapi.app.excpetions.RequestException import AuthException, PermissionException
+from webapi.app.handler.fatcory import SakuraResponse
+from webapi.app.middleware.Jwt import UserToken
+from webapi.app.models import async_session
+from webapi.config import Config
+
+FORBIDDEN = "对不起,您没有足够的权限"
+
+
+class Permission:
+    def __init__(self, role: int = Config.MEMBER):
+        self.role = role
+
+    async def __call__(self, token: str = Header(...)):
+        if not token:
+            raise AuthException(status.HTTP_200_OK, "用户信息身份认证失败,请检查！")
+        try:
+            user_info = UserToken.parse_token(token)
+            if user_info.get("role", 0) < self.role:
+                raise PermissionException(status.HTTP_200_OK, FORBIDDEN)
+            user = await UserDao.query_user(user_info['id'])
+            if user is None:
+                raise Exception("用户不存在")
+            user_info = SakuraResponse.model_to_list(user, "password")
+        except PermissionException as e:
+            raise e
+        except Exception as e:
+            raise AuthException(status.HTTP_200_OK, str(e)) from e
+        return user_info
+
+
+async def get_session():
+    """
+    获取异步session
+    :return:
+    """
+    async with async_session() as session:
+        yield session
