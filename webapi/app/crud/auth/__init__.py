32f8b41335d970faa7b43f302644f7fe70e83fb8
luck_baby <67427627+ranyong1997@users.noreply.github.com>
2022-07-23 08:45:08 +0800

    ❄️feat(Added __init__.py):新增 __init__.py

---
+++ webapi/app/crud/auth/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/7/22 10:16
+# @Author  : 冉勇
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: PyCharm
+# @desc    :
---
+++ webapi/app/excpetions/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/7/20 11:34
+# @Author  : 冉勇
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: PyCharm
+# @desc    :
