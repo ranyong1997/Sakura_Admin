6133db848ca3bb59b40c9b5c33b2d22d8fe3dd42
luck_baby <67427627+ranyong1997@users.noreply.github.com>
2022-07-23 08:37:55 +0800

    ❄️feat(New Environment Configuration):新增环境配置

---
+++ webapi/app/routers/config/environment.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/7/20 11:41
+# @Author  : 冉勇
+# @Site    : 
+# @File    : environment.py
+# @Software: PyCharm
+# @desc    : 环境配置
+from fastapi import APIRouter,Depends
+from webapi.app.c
