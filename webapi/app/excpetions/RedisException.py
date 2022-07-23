578d3e8cd7e1d777bcc98a88c671488f5d0053bb
luck_baby <67427627+ranyong1997@users.noreply.github.com>
2022-07-23 08:41:26 +0800

    ❄️feat(Added redis exception):新增redis异常

---
+++ webapi/app/excpetions/RedisException.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/7/20 13:57
+# @Author  : 冉勇
+# @Site    : 
+# @File    : RedisException.py
+# @Software: PyCharm
+# @desc    : Redis异常
+class RedisException(Exception):
+    pass
