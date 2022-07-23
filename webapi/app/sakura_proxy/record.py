4672a064cd8c350d4507cd4952042590fd3f7c36
luck_baby <67427627+ranyong1997@users.noreply.github.com>
2022-07-23 08:39:05 +0800

    ❄️feat(Adding Traffic Recording):新增流量录制

---
+++ webapi/app/sakura_proxy/record.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/7/21 16:49
+# @Author  : 冉勇
+# @Site    : 
+# @File    : record.py
+# @Software: PyCharm
+# @desc    : 流量录制-->生成case功能
+import asyncio
+import json
+import re
+from webapi.app.
