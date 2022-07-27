7247725c872fe4b8ff5772ff828b01e174d03bbf
luck_baby <67427627+ranyong1997@users.noreply.github.com>
2022-07-27 15:25:54 +0800

    🎊feat(Added RedisConfig(Dao) packaging):新增Redis配置Dao封装

---
+++ webapi/app/crud/config/RedisConfigDao.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/7/27 15:19
+# @Author  : 冉勇
+# @Site    : 
+# @File    : RedisConfigDao.py
+# @Software: PyCharm
+# @desc    : ReidsDao(逻辑)
+from webapi.app.crud import Mapper
+from webapi.app.middleware.RedisManager import SakuraRedisManager, RedisHelper
+from webapi.app.models.redis_config import SakuraRedis
+from webapi.app.utils.decorator import dao
+from webapi.app.utils.logger import Log
+
+
+@dao(SakuraRedis, Log("SakuraRedisConfigDao"))
+class SakuraRedisConfigDao(Mapper):
+    @staticmethod
+    async def execute_command(command: str, **kwargs):
+        try:
+            redis_config = await SakuraRedisConfigDao.query_record(**kwargs)
+            if redis_config is None:
+                raise Exception("Redis配置不存在")
+            if not redis_config.cluster:
+                client = SakuraRedisManager.get_single_client(redis_config.id, redis_config.addr, redis_config.password,
+                                                              redis_config.db)
+            else:
+                client = SakuraRedisManager.get_cluster_client(redis_config.id, redis_config.addr)
+            return await RedisHelper.execute_command(client, command)
+        except Exception as e:
+            raise Exception(f"执行redis命令出错:{e}") from e
