e1a5b4a6f8dbc30a560b438ea5e493d7e180f4e9
luck_baby <67427627+ranyong1997@users.noreply.github.com>
2022-07-27 10:52:40 +0800

    🎐feat(Adding a database form):新增数据库表单

---
+++ webapi/app/schema/database.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/7/27 10:49
+# @Author  : 冉勇
+# @Site    : 
+# @File    : database.py
+# @Software: PyCharm
+# @desc    : 数据库表单
+from pydantic import validator, BaseModel
+from webapi.app.schema.base import SakuraModel
+
+
+class DataBaseForm(BaseModel):
+    id: int = None
+    name: str
+    host = str
+    port: int = None
+    username: str
+    password: str
+    database: str
+    sql_type: int
+    env: int
+
+    @validator("name", "host", "port", "username", "password", "database", "sql_type", "env")
+    def data_not_empty(cls, v):
+        return SakuraModel.not_empty(v)
