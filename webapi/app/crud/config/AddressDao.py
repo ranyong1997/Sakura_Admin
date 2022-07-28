53e750be9acbe0a1b9324b2d33b10e91d17ca587
luck_baby <67427627+ranyong1997@users.noreply.github.com>
2022-07-28 09:51:24 +0800

    🎁feat(Added Address(Dao) packaging):新增网关Dao封装

---
+++ webapi/app/crud/config/AddressDao.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/7/28 09:32
+# @Author  : 冉勇
+# @Site    : 
+# @File    : AddressDao.py
+# @Software: PyCharm
+# @desc    : 网关Dao(逻辑)
+from sqlalchemy import select
+from webapi.app.crud import Mapper
+from webapi.app.models import async_session
+from webapi.app.models.address import SakuraGateway
+from webapi.app.utils.decorator import dao
+from webapi.app.utils.logger import Log
+
+
+@dao(SakuraGateway, Log("SakuraRedisConfigDao"))
+class SakuraGatewayDao(Mapper):
+    @staticmethod
+    async def query_gateway(env, name):
+        async with async_session() as session:
+            query = await session.execute(
+                select(SakuraGateway).where(SakuraGateway.deleted_at == 0, SakuraGateway.env == env,
+                                            SakuraGateway.name == name)
+            )
+            data = query.scalars().first()
+            if data is None:
+                raise Exception(f"此环境没有网关配置:{name}")
+            return data.gateway
