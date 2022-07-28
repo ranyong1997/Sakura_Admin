ff2c1bd1ea55aa4a464d085d6df1643195a7f6e3
luck_baby <67427627+ranyong1997@users.noreply.github.com>
2022-07-28 16:37:34 +0800

    🍕feat(Added Oss[crud] router):新增oss路由增删改查

---
+++ webapi/app/routers/oss/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/7/28 11:39
+# @Author  : 冉勇
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: PyCharm
+# @desc    :
+from webapi.app.routers.oss.oss_file import router
