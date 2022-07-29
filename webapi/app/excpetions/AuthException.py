7b544363c7eacb6790e9699ecaf76b5bd53141f4
luck_baby <67427627+ranyong1997@users.noreply.github.com>
2022-07-29 10:32:57 +0800

    🧚‍feat(Added AuthException):新增身份验证异常处理类

---
+++ webapi/app/excpetions/AuthException.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/7/29 10:31
+# @Author  : 冉勇
+# @Site    : 
+# @File    : AuthException.py
+# @Software: PyCharm
+# @desc    : 身份验证异常处理
+
+class AuthException(Exception):
+    pass
