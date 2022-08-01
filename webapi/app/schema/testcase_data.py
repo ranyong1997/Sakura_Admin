7ba3271e41d2988e0c9bcd9ee54d865a617640f9
luck_baby <67427627+ranyong1997@users.noreply.github.com>
2022-08-01 16:32:27 +0800

    🍆feat(Added Test case data form):新增测试用例数据表单

---
+++ webapi/app/schema/testcase_data.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/8/1 16:29
+# @Author  : 冉勇
+# @Site    : 
+# @File    : testcase_data.py
+# @Software: PyCharm
+# @desc    : 测试用例数据表单
+from pydantic import BaseModel, validator
+from webapi.app.schema.base import SakuraModel
+
+
+class SakuraTestcaseDateForm(BaseModel):
+    id: int = None
+    case_id: int = None
+    name: str
+    json_data: str
+    env: int
+
+    @validator("env", "name", "json_data")
+    def name_not_empty(cls, v):
+        return SakuraModel.not_empty(v)
