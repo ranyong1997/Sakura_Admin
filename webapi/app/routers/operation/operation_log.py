12162272dcada883d38957410b2bf38d5cd0a945
luck_baby <67427627+ranyong1997@users.noreply.github.com>
2022-08-03 15:51:58 +0800

    🥞feat(Added Get the number of user projects):新增获取用户操作记录热力图以及参与的项目数量

--- webapi/app/routers/operation/operation_log.py
+++ webapi/app/routers/operation/operation_log.py
@@ -21,10 +21,27 @@ router = APIRouter(perfix="/operation")
 @router.get("/list")
 async def list_user_operation(start_time: str, end_time: str, user_id: int, tag: str = None, _=Depends(Permission())):
     try:
-        start = datetime.strftime(start_time, "%Y-%m-%d")
-        end = datetime.strftime(end_time, "%Y-%m-%d")
+        start = datetime.strptime(start_time, "%Y-%m-%d")
+        end = datetime.strptime(end_time, "%Y-%m-%d")
         records = await SakuraOperationDao.list_record(user_id=user_id, tag=tag, condition=[
             SakuraOperationLog.operate_time.between(start, end)], _sort=[desc(SakuraOperationLog.operate_time)])
         return SakuraResponse.records(records)
+    except Exception as e:
+        return SakuraResponse.failed(e)
+
+
+# 获取用户操作记录热力图以及参与的项目数量
+@router.get("/count")
+async def list_user_activities(user_id: int, start_time: str, end_time: str, _=Depends(Permission())):
+    try:
+        start = datetime.strptime(start_time, "%Y-%,-%d")
+        end = datetime.strptime(end_time, "%Y-%m-%d")
+        records = await SakuraOperationDao.count_user_activities(user_id, start, end)
+        ans = []
+        for r in records:
+            # 解包日期和数量
+            date, count = r
+            ans.append(dict(date=date.strftime("%Y-%m-%d"), count=count))
+        return SakuraResponse.success(ans)
     except Exception as e:
         return SakuraResponse.failed(e)
