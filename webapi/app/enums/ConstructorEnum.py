b0438fab8b37bcda85b4b56e227ba8741ece77fa
luck_baby <67427627+ranyong1997@users.noreply.github.com>
2022-08-03 17:42:38 +0800

    🍗feat(Added Constructor enumeration):新增构造函数枚举

---
+++ webapi/app/enums/ConstructorEnum.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/8/3 17:16
+# @Author  : 冉勇
+# @Site    : 
+# @File    : ConstructorEnum.py
+# @Software: PyCharm
+# @desc    : 构造函数枚举->前置条件类型
+from enum import IntEnum
+
+
+class Constructor(IntEnum):
+    testcase = 0
+    sql = 0
+    redis = 2
+    py_script = 3
+    http = 4
