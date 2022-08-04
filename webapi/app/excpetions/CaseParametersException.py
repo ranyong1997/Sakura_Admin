45cdbf94c14620f309ccf407f7e177f9c8708e0d
luck_baby <67427627+ranyong1997@users.noreply.github.com>
2022-08-04 17:21:42 +0800

    🍨feat(Added Use case parameters are abnormal):新增用例参数异常

---
+++ webapi/app/excpetions/CaseParametersException.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/8/4 17:20
+# @Author  : 冉勇
+# @Site    : 
+# @File    : CaseParametersException.py
+# @Software: PyCharm
+# @desc    : 用例参数异常
+class CaseParametersException(Exception):
+    pass
