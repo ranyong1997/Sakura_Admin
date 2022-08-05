b68dad5e649cf137945b9b99bb378d88e8ec47bf
luck_baby <67427627+ranyong1997@users.noreply.github.com>
2022-08-05 11:21:16 +0800

    🍾feat(Added parameter initialization):新增参数初始化

---
+++ webapi/app/core/paramters/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/8/4 17:17
+# @Author  : 冉勇
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: PyCharm
+# @desc    :
+from webapi.app.core.paramters.jsonpath_parser import JSONPathParser
+from webapi.app.core.paramters.kv_parser import HeaderParser, CookieParser
+from webapi.app.core.paramters.regex_parser import RegexParser
+from webapi.app.core.paramters.status_code_parser import StatusCodeParser
+from webapi.app.enums.CaseParametersEnum import CaseParametersEnum
+
+
+def ParameterParser(parameter_type: CaseParametersEnum):
+    if parameter_type == CaseParametersEnum.TEXT:
+        return RegexParser.parse
+    if parameter_type == CaseParametersEnum.JSON:
+        return JSONPathParser.parse
+    if parameter_type == CaseParametersEnum.HEADER:
+        return HeaderParser.parse
+    if parameter_type == CaseParametersEnum.COOKIE:
+        return CookieParser.parse
+    if parameter_type == CaseParametersEnum.STATUS_CODE:
+        return StatusCodeParser.parse
