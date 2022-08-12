e9f4a3cb388ddd019a083a93e6266935430ef4d1
luck_baby <67427627+ranyong1997@users.noreply.github.com>
2022-08-12 17:26:16 +0800

    🐒feat(Added GenerateException):新增产生异常

---
+++ webapi/app/excpetions/convert/GenerateException.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/8/12 17:25
+# @Author  : 冉勇
+# @Site    : 
+# @File    : GenerateException.py
+# @Software: PyCharm
+# @desc    : 产生异常
+class GenerateException(Exception):
+    pass
