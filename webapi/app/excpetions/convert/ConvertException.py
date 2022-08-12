32ac4f5dfb1ad23a6081a2c665c2638dbaab7c33
luck_baby <67427627+ranyong1997@users.noreply.github.com>
2022-08-12 14:31:18 +0800

    🎗️feat(Adding Conversion exception handling):新增转换异常处理

---
+++ webapi/app/excpetions/convert/ConvertException.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/8/12 14:30
+# @Author  : 冉勇
+# @Site    : 
+# @File    : ConvertException.py
+# @Software: PyCharm
+# @desc    : 转换异常处理
+class HarConvertException(Exception):
+    pass
