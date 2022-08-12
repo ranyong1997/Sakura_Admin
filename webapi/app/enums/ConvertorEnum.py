275bb7905100e6a25ec34e5a7c6f98e668ff1077
luck_baby <67427627+ranyong1997@users.noreply.github.com>
2022-08-12 14:54:37 +0800

    🎖️️feat(Adding Convert the enumeration):新增转换枚举

---
+++ webapi/app/enums/ConvertorEnum.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/8/12 14:53
+# @Author  : 冉勇
+# @Site    : 
+# @File    : ConvertorEnum.py
+# @Software: PyCharm
+# @desc    : 转换枚举
+from enum import IntEnum
+
+
+class CaseConvertorType(IntEnum):
+    har = 1
