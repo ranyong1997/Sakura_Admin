55c1cca001b83f7eb2f466103c702aecb7e0bfc4
luck_baby <67427627+ranyong1997@users.noreply.github.com>
2022-08-26 15:07:04 +0800

    🕷update(update code):更新代码 移除(init)文件

--- webapi/app/routers/request/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 2022/8/1 11:12
-# @Author  : 冉勇
-# @Site    : 
-# @File    : __init__.py.py
-# @Software: PyCharm
-# @desc    :
--- webapi/static/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 2022/7/6 17:32
-# @Author  : 冉勇
-# @Site    : 
-# @File    : __init__.py.py
-# @Software: PyCharm
-# @desc    :
