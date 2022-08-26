df757c8cb0b269cadf30c3117026eec6c4ab03f2
luck_baby <67427627+ranyong1997@users.noreply.github.com>
2022-08-26 15:11:01 +0800

    🎃feat(Added secret key):新增密钥

---
+++ webapi/app/utils/des.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/8/23 10:34
+# @Author  : 冉勇
+# @Site    : 
+# @File    : des.py
+# @Software: PyCharm
+# @desc    : 密钥
+import binascii
+from pyDes import des, CBC, PAD_PKCS5
+
+# 密钥
+KEY = "sakurapwd"
+
+
+class Des(object):
+    @staticmethod
+    def des_encrypt(s):
+        """
+        DES 加密
+        :param s: 原始字符串
+        :return: 加密后字符串，16进制
+        """
+        secret_key = KEY  # 密码
+        iv = secret_key  # 偏移
+        # secret_key:加密密钥，CBC:加密模式，iv:偏移, padmode:填充
+        des_obj = des(secret_key, CBC, iv, pad=None, padmode=PAD_PKCS5)
+        # 返回为字节
+        secret_bytes = des_obj.encrypt(s, padmode=PAD_PKCS5)
+        # 返回为16进制
+        return binascii.b2a_hex(secret_bytes).decode()
+
+    @staticmethod
+    def des_decrypt(s):
+        """
+        DES 解密
+        :param s: 加密后的字符串，16进制
+        :return:  解密后的字符串
+        """
+        secret_key = KEY
+        iv = secret_key
+        des_obj = des(secret_key, CBC, iv, pad=None, padmode=PAD_PKCS5)
+        decrypt_str = des_obj.decrypt(binascii.a2b_hex(s), padmode=PAD_PKCS5)
+        return decrypt_str.decode()
