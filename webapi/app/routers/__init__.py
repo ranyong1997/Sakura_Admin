4ee5612558956f668a035e9da37c44cc22cb9545
luck_baby <67427627+ranyong1997@users.noreply.github.com>
2022-09-03 10:29:52 +0800

    🐞Fix:(Fixed login token): 修复登录token User object is not iterable

--- webapi/app/routers/__init__.py
+++ webapi/app/routers/__init__.py
@@ -6,6 +6,7 @@
 # @File    : __init__.py.py
 # @Software: PyCharm
 # @desc    : 路由初始化[身份鉴权]
+import asyncio
 from fastapi import Header
 from starlette import status
 from webapi.app.crud.auth.UserDao import UserDao
@@ -13,9 +14,10 @@ from webapi.app.excpetions.RequestException import AuthException, PermissionExce
 from webapi.app.handler.fatcory import SakuraResponse
 from webapi.app.middleware.Jwt import UserToken
 from webapi.app.models import async_session
+from webapi.app.utils.internal import synchronize_async_helper
 from webapi.config import Config
 
-FORBIDDEN = "对不起,您没有足够的权限"
+FORBIDDEN = "对不起, 你没有足够的权限"
 
 
 class Permission:
@@ -24,7 +26,7 @@ class Permission:
 
     async def __call__(self, token: str = Header(...)):
         if not token:
-            raise AuthException(status.HTTP_200_OK, "用户信息身份认证失败,请检查！")
+            raise AuthException(status.HTTP_200_OK, "用户信息身份认证失败, 请检查")
         try:
             user_info = UserToken.parse_token(token)
             if user_info.get("role", 0) < self.role:
@@ -32,11 +34,11 @@ class Permission:
             user = await UserDao.query_user(user_info['id'])
             if user is None:
                 raise Exception("用户不存在")
-            user_info = SakuraResponse.model_to_list(user, "password")
+            user_info = SakuraResponse.model_to_dict(user, "password")
         except PermissionException as e:
             raise e
         except Exception as e:
-            raise AuthException(status.HTTP_200_OK, str(e)) from e
+            raise AuthException(status.HTTP_200_OK, str(e))
         return user_info
 
 
